ac8093bf9d9d12d711d9255668dfe27dafe53771
mszawerd <01169246@pw.edu.pl>
2024-04-19 16:19:22 +0200

    add playlist tests

---
+++ test/test_Playlist.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+#include "../src/Playlist.h"
+#define SAMPLE_RATE 44100
+
+TEST(Playlist, ShouldInitPlaylistBeEmpty) {
+    // given
+    Playlist playlist;
+
+    // then
+    ASSERT_FALSE(playlist.current().has_value());
+}
+
+TEST(Playlist, AddSongToPlaylist) {
+    // given
+    auto song = std::make_shared<Song>(nullptr, SAMPLE_RATE);
+    Playlist playlist;
+
+    // when
+    playlist.addSong(song);
+    playlist.next();
+
+    // then
+    ASSERT_EQ(playlist.current(), song);
+}
+
+TEST(Playlist, NavigateThroughPlaylist) {
+    // given
+    auto song1 = std::make_shared<Song>(nullptr, SAMPLE_RATE);
+    auto song2 = std::make_shared<Song>(nullptr, SAMPLE_RATE*2);
+    Playlist playlist;
+
+    // when
+    playlist.addSong(song1);
+    playlist.addSong(song2);
+    playlist.next();
+    playlist.next();
+
+    // then
+    ASSERT_EQ(playlist.current(), song2);
+    ASSERT_EQ(playlist.previous(), song1);
+}
